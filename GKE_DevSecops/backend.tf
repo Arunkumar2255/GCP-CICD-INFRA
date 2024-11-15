1a7d42720379e5fb9d6825405ebc09b09f44584d
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-11-15 11:25:16 +0530

    Update backend.tf

--- GKE_DevSecops/backend.tf
+++ GKE_DevSecops/backend.tf
@@ -2,5 +2,6 @@ terraform {
  backend "gcs" {
    bucket  = "application-tfstate-121321"
    prefix  = "terraform/state"
+credentials = "/home/runner/work/GCP-CICD-INFRA/GCP-CICD-INFRA/gha-creds-4e695fa7d6018518.json"
  }
 }
